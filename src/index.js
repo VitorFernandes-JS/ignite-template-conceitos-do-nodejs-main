69afb2b1dd91cd6ab0195ea6bde8ce3ceb5e1ec5
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2023-01-30 15:09:26 -0300

    fix: all routes completed

--- src/index.js
+++ src/index.js
@@ -9,13 +9,16 @@ app.use(express.json());
 
 function checksExistsUserAccount(request, response, next) {
   const { username } = request.headers;
+
   const user = users.find((user) => user.username === username);
 
   if (!user) {
-    return response.status(400);
+    return response.status(404).json({
+      error: "Usuário não encontrado",
+    });
   }
 
-  request.user = user; // fazemos isso para termos acesso ao user fora da middleware
+  request.user = user;
 
   return next();
 }
@@ -23,14 +26,22 @@ function checksExistsUserAccount(request, response, next) {
 app.post("/users", (request, response) => {
   const { name, username } = request.body;
 
-  users.push({
+  const userExists = users.find((user) => user.username === username);
+
+  if (userExists) {
+    return response.status(400).json({ error: "Usuário já cadastrado!" });
+  }
+
+  const user = {
     id: uuidv4(),
     name,
     username,
     todos: [],
-  });
+  };
 
-  response.json({ message: "Usuário criado!" });
+  users.push(user);
+
+  response.json(user).status(201);
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
@@ -51,20 +62,55 @@ app.post("/todos", checksExistsUserAccount, (request, response) => {
     created_at: new Date(),
   };
 
-  user.todos.push(todo)
-  response.json(todo)
+  user.todos.push(todo);
+  return response.status(201).json(todo);
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+
+  const todo = user.todos.find(item => item.id === id) 
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo não existe"})
+  }
+  
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.status(todo)
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(item => item.id === id) 
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo não existe"})
+  }
+  
+  todo.done = true;
+
+  return response.json(todo)
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todoIndex = user.todos.findIndex(item => item.id === id) 
+
+  if (todoIndex === -1) {
+    return response.status(404).json({ error: "Todo não existe"})
+  }
+
+  user.todo.splice(todoIndex, 1)
+  
+  return response.status(204).json()
 });
 
 module.exports = app;
