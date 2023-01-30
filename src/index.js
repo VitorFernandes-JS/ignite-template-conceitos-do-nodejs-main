b8e737a96dec17ad20d9d81fc825135ba700e7ac
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2023-01-30 11:32:32 -0300

    fix: create users

--- src/index.js
+++ src/index.js
@@ -36,11 +36,23 @@ app.post("/users", (request, response) => {
 app.get("/todos", checksExistsUserAccount, (request, response) => {
   const { user } = request;
 
-  return response.json(user.todos)
+  response.json(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  };
+
+  user.todos.push(todo)
+  response.json(todo)
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
